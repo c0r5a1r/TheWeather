b59b922433b6f62a96d29d612e39656c69a6b96d
c0r5a1r <orsarabun@gmail.com>
2015-09-04 14:07:51 +0000

    майже закінчений JS + підкореговані стилі

--- js/javascript.js
+++ js/javascript.js
@@ -1,12 +1,39 @@
 $(function() {
 
-    getWeatherData('ua', showDate, showErr);
+   $('#btnWeather').click(function () {
+        getWeatherByCity('en', showDate, showErr, $('#inputCityName').val());
+    });
+    $('#inputCityName').keypress(function(e) {
+        var ENTER_KEY_CODE = 13;
+        if ( e.which === ENTER_KEY_CODE ) 
+        {
+            $('#btnWeather').trigger('click');
+            return false;
+        }
+    });    
+  
+    getWeatherData('en', showDate, showErr);
+  
 
     function showDate (data) {
         var city = data.city.name;
         var country = data.city.country;
+        var tempDay = data.list[0].temp.day;
+        var tempNight = data.list[0].temp.night;
+        var description = data.list[0].weather[0].description;
+        var windSpeed = data.list[0].speed;
+        var humidity = data.list[0].humidity;
+        var pressure = data.list[0].pressure;
+        var HECTOPASCAL = 0.75006375541921;
+        var weathericon = data.list[0].weather[0].icon; 
         
-        $('#location').html(city + ', <b>' + country + '</b>');
+        $('#location').html(city + ', <strong>' + country + '</strong>');
+        $('#tempDay').html(tempDay);
+        $('#tempNight').html(tempNight);
+        $('#description').html(description);
+        $('#weatherIcon').html('<img src="images/sighs/'+ weathericon + '.png" />');
+        
+    
     }
         
     function showErr(message) {
