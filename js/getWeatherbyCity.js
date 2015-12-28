3e1aad91ec9f63e077881dd8cf5a44469a3d0e42
c0r5a1r <orsarabun@gmail.com>
2015-12-28 14:21:39 +0000

    appid added

--- js/getWeatherbyCity.js
+++ js/getWeatherbyCity.js
@@ -1,7 +1,7 @@
 function getWeatherByCity(lang, fnOK, fnError, cityName) {
     $.getJSON(
         'http://api.openweathermap.org/data/2.5/forecast/daily?q=' 
-        + cityName + '&cnt=16&units=metric' + '&lang=' + lang + '&callback=?',
+        + cityName + '&cnt=16&units=metric&APPID=2b45317c04d69036d30782582b0a1462' + '&lang=' + lang + '&callback=?',
         function (data) {
             fnOK.call(this, data);
         }
